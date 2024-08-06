3accb278fff8acf943c987c584fe005ecca5e844
maksimkaJCHK <maksimkazhuchkov@gmail.com>
2024-08-06 15:59:49 +0300

    доделал информационный блок

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useGameContext } from '@/context/mainContext';
 
 import './info.scss';
@@ -6,9 +6,24 @@ import './info.scss';
 const Info = () => {
   const { count, isGameOver } = useGameContext();
 
+  const infoText = useMemo(() => {
+    if (isGameOver) return `Congratulations you completed the game in ${count} moves.`;
+    if (count === 0) return `Make the first move.`;
+
+    return `You made ${count} moves.`
+  }, [count, isGameOver]);
+
+  const styleInfo = useMemo(() => {
+    const typeClass = ['info'];
+
+    if (isGameOver) typeClass.push('isGameOver') ;
+
+    return typeClass.join(' ');
+  }, [isGameOver]);
+
   return (
-    <div className = "info">
-      Congratulations you completed the game in { count } moves
+    <div className = { styleInfo }>
+      { infoText }
     </div>
   )
 }
